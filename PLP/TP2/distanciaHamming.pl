b5cdfc5052500109e2bd691a81b74ebd51a0d0d2
martincammi <martincammi@e665703e-009a-9b55-7c73-0e67ad5d86b4>
2013-05-31 22:50:50 +0000

    Corregido para que chequee que sean valores entre 0 y 1.

--- PLP/TP2/distanciaHamming.pl
+++ PLP/TP2/distanciaHamming.pl
@@ -11,4 +11,10 @@ entre(X,Y,Z) :- X<Y, Xm1 is X+1, entre(Xm1, Y, Z).
 distanciaHamming([], [], 0).
 distanciaHamming([X|XS], [Y|YS], N) :- entre(0,1,V), Y is V, distanciaHamming(XS,YS,ACUM), N is ACUM + abs(Y-X).
 
+test1(N) :- distanciaHamming([],[0],N).
+test2(N) :- distanciaHamming([0,1],[0,1],N).
+test3(N) :- distanciaHamming([0,1,0],[0,0,1],N). 
+test4(X,Y,N) :- distanciaHamming([0,1,0],[X,Y,1],N). 
+test5(X) :- distanciaHamming([0,1,0],X,2).
+test6(N) :- distanciaHamming([plp],[plp],N).
 
