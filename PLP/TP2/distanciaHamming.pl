f8b1dbbb792d185abb68ec1d168b82ffb4654e64
martincammi <martincammi@e665703e-009a-9b55-7c73-0e67ad5d86b4>
2013-06-01 03:07:07 +0000

    Armando el ejercicio2

--- PLP/TP2/distanciaHamming.pl
+++ PLP/TP2/distanciaHamming.pl
@@ -11,10 +11,33 @@ entre(X,Y,Z) :- X<Y, Xm1 is X+1, entre(Xm1, Y, Z).
 distanciaHamming([], [], 0).
 distanciaHamming([X|XS], [Y|YS], N) :- entre(0,1,V), Y is V, distanciaHamming(XS,YS,ACUM), N is ACUM + abs(Y-X).
 
-test1(N) :- distanciaHamming([],[0],N).
-test2(N) :- distanciaHamming([0,1],[0,1],N).
-test3(N) :- distanciaHamming([0,1,0],[0,0,1],N). 
-test4(X,Y,N) :- distanciaHamming([0,1,0],[X,Y,1],N). 
-test5(X) :- distanciaHamming([0,1,0],X,2).
-test6(N) :- distanciaHamming([plp],[plp],N).
+testdh1(N) :- distanciaHamming([],[0],N).
+testdh2(N) :- distanciaHamming([0,1],[0,1],N).
+testdh3(N) :- distanciaHamming([0,1,0],[0,0,1],N). 
+testdh4(X,Y,N) :- distanciaHamming([0,1,0],[X,Y,1],N). 
+testdh5(X) :- distanciaHamming([0,1,0],X,2).
+testdh6(N) :- distanciaHamming([plp],[plp],N).
 
+
+%distPref(+S, +T, ?D)
+%distPref([], YS,N) :- binaria(YS), length(YS,N).
+%distPref(XS, [],N) :- binaria(XS), length(XS,N). 
+%distPref([X|XS], [Y|YS], N) :- entre(0,1,X), X==Y, distPref(XS, YS, N).
+%distPref([X|XS], [Y|YS], N) :- entre(0,1,X), entre(0,1,V), Y is V, X=\=Y, length(YS,LYS), length(XS,LXS), N is (LYS + LXS + 2).
+
+distPref([], YS,N) :- length(YS,N). %binaria(YS), length(YS,N).
+distPref(XS, [],N) :- length(XS,N).%binaria(XS), length(XS,N). 
+distPref([X|XS], [Y|YS], N) :- entre(0,1,X), entre(0,1,V), Y is V, X==Y, distPref(XS, YS, N).
+distPref([X|XS], [Y|YS], N) :- entre(0,1,X), entre(0,1,V), Y is V, X=\=Y, length(YS,LYS), length(XS,LXS), N is (LYS + LXS + 2).
+
+%genBinaria(?L,+N)
+%binaria([], 0).
+%binaria([X|XS], N) :- length([X|XS],LengthLista), LengthLista<=N, entre(0,1,X), K is N-1, binaria(XS,K).
+binaria([], 0).
+binaria([Y|YS], N) :- entre(0,1,V), Y is V, binaria(YS,ACUM), N is (ACUM + 1).
+
+%desde(+X,-Y)
+%desde(X,X).
+%desde(X,Y) :- Z is X+1, desde(Z,Y).
+
+%testdp1(N) :- distPref([0,1,1],[0,1,1,1],N).
