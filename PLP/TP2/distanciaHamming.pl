01e7a2c503d845f3ca5d419c5c3e135aabc304a2
anafelisatti@gmail.com <anafelisatti@gmail.com@e665703e-009a-9b55-7c73-0e67ad5d86b4>
2013-06-02 21:32:59 +0000

    lalalalala no anda nada

--- PLP/TP2/distanciaHamming.pl
+++ PLP/TP2/distanciaHamming.pl
@@ -37,7 +37,7 @@ testdh6(N) :- distanciaHamming([plp],[plp],N).
 %7 -- defino / valido el 1 er elemento de [Y|YS] y de ser distinto necesito generar el resto de la lista (N no definida)
 %8 -- defino / valido el 1 er elemento de [Y|YS] y de ser distinto necesito generar el resto de la lista (N definida)
 
-distPref(X,Y,N) :- not(ground(Y)), not(ground(N)), desde(0,N), distPref(X,Y,N).
+distPref(X,Y,N)   :- not(ground(Y)), not(ground(N)), desde(0,N), distPref(X,Y,N).
 distPref([],YS,N) :- ground(YS), length(YS,N), binaria(YS,N). %1
 distPref([],YS,N) :- not(ground(YS)), binaria(YS,N). %3 
 distPref(XS, [],N):- length(XS,N), N>0, binaria(XS,N). %4	
@@ -45,7 +45,7 @@ distPref([X|XS],[Y|YS],N) :- entre(0,1,X), entre(0,1,Y), X==Y, distPref(XS,YS,N)
 distPref([X|XS],[Y|YS],N) :- entre(0,1,X), entre(0,1,Y), X=\=Y,     ground(YS),  length(XS,LengthXS), binaria(XS,LengthXS), length(YS,LengthYS), binaria(YS, LengthYS),	N is (LengthXS+LengthYS+2). %6
 distPref([X|XS],[Y|YS],N) :- entre(0,1,X), entre(0,1,Y), X=\=Y, not(ground(YS)), length(XS,LengthXS), ACUM is (N-LengthXS-2), distPref([],YS,ACUM).	%8		  
 
-
+%1 si la lista no instanciada ya tiene una longitud fija, cortamos al superar la cota del resultado.
 %Binaria(?L,+N)
 binaria([], 0).
 binaria([Y|YS], N) :- entre(0,1,V), Y is V, ACUM is (N - 1), ACUM >= 0, binaria(YS,ACUM).
@@ -53,3 +53,18 @@ binaria([Y|YS], N) :- entre(0,1,V), Y is V, ACUM is (N - 1), ACUM >= 0, binaria(
 %desde(+X,-Y)
 desde(X,X).
 desde(X,Y) :- Z is X+1, desde(Z,Y).
+
+%distEd(+S,+T,?D)
+distEd([],T,D) :- length(T,D), binaria(T,D).
+distEd(S,[],D) :- length(S,D), binaria(S,D).
+distEd([X|XS],[Y|YS],D) :- 	distEd(XS,YS,Intercambiar), NewIntercambiar is (Intercambiar + abs(X -Y)),	%intercambio elementos
+							distEd([X|XS],YS,Eliminar), NewEliminar 	is (Eliminar + 1),				%elimino elemento
+							distEd(XS,[Y|YS],Agregar),  NewAgregar 		is (Agregar + 1),				%agrego elemento
+							minimo(NewIntercambiar,NewEliminar,NewAgregar,D).
+							
+minimo(A,B,C,Res) :- A=<B, B=<C, Res is A.
+minimo(A,B,C,Res) :- A=<C, C=<B, Res is A.
+minimo(A,B,C,Res) :- B=<A, A=<C, Res is B.
+minimo(A,B,C,Res) :- B=<C, C=<A, Res is B.
+minimo(A,B,C,Res) :- C=<A, A=<B, Res is C.
+minimo(A,B,C,Res) :- C=<B, B=<A, Res is C.
