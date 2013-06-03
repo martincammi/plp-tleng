cac554043af10ba0d52e08a103b5a6c7bbabbba4
martincammi <martincammi@e665703e-009a-9b55-7c73-0e67ad5d86b4>
2013-06-03 22:42:22 +0000

    Correciones al Ejercicio 2

--- PLP/TP2/distanciaHamming.pl
+++ PLP/TP2/distanciaHamming.pl
@@ -18,32 +18,18 @@ testdh4(X,Y,N) :- distanciaHamming([0,1,0],[X,Y,1],N).
 testdh5(X) :- distanciaHamming([0,1,0],X,2).
 testdh6(N) :- distanciaHamming([plp],[plp],N).
 
-%distPref(+S, ?T, ?D)
-%distPref([],YS,N) :- ground(YS), length(YS,N), binaria(YS,N). %1
-%distPref([],YS,N) :- not(ground(YS)), not(ground(N)), desde(0,M), binaria(YS,M), N is M. %2
-%distPref([],YS,N) :- not(ground(YS)), ground(N), binaria(YS,N). %3 
-%distPref(XS, [],N):- length(XS,N), N>0, binaria(XS,N). %4	
-%distPref([X|XS],[Y|YS],N) :- entre(0,1,X), entre(0,1,Y), X==Y, distPref(XS,YS,N). %5
-%distPref([X|XS],[Y|YS],N) :- entre(0,1,X), entre(0,1,Y), X=\=Y,     ground(YS),  length(XS,LengthXS), length(YS,LengthYS), 	N is (LengthXS+LengthYS+2). %6
-%distPref([X|XS],[Y|YS],N) :- entre(0,1,X), entre(0,1,Y), X=\=Y, not(ground(YS)), not(ground(N)), length(XS,LengthXS), distPref([],YS,ACUM), N is (ACUM+LengthXS+2). %7
-%distPref([X|XS],[Y|YS],N) :- entre(0,1,X), entre(0,1,Y), X=\=Y, not(ground(YS)), 	  ground(N), length(XS,LengthXS), ACUM is (N-LengthXS-2), distPref([],YS,ACUM).	%8		  
 
-%1 -- si esta instancia ys => validar calcular longitud
-%2 -- sino => generar todas las listas e ir devolviendo su longitud (tbm generada)
-%3 -- sino => generar todas las listas de longitud N
-%4 -- calcula la longitud de XS validando que sea una lista binaria (no vacia)
-%5 -- elimina prefijo instanciando Y como cada item del prefijo
-%6 -- si ambas listas estan definidas direcamente calcula sus longitudes
-%7 -- defino / valido el 1 er elemento de [Y|YS] y de ser distinto necesito generar el resto de la lista (N no definida)
-%8 -- defino / valido el 1 er elemento de [Y|YS] y de ser distinto necesito generar el resto de la lista (N definida)
+% caso infinito, sin n definido genero todos los posibles Ns
+distPref(X,Y,N) :- not(is_list(Y)), not(ground(N)), desde(0,N), distPref(X,Y,N). 
+distPref([],YS,N) :- not(is_list(YS)), ground(N), binaria(YS,N).
+distPref([X|XS],[Y|YS],N) :- not(is_list(YS)), ground(N), entre(0,1,X), entre(0,1,Y), X==Y, distPref(XS,YS,N).
+distPref([X|XS],[Y|YS],N) :- not(is_list(YS)), ground(N), entre(0,1,X), entre(0,1,Y), X=\=Y, length(XS,LengthXS), binaria(XS,LengthXS), ACUM is (N-LengthXS-2), distPref([],YS,ACUM).	
+% caso finito
+distPref([],YS,N) :- is_list(YS), length(YS,N), binaria(YS,N).
+distPref(XS, [],N):- length(XS,N), N>0, binaria(XS,N). %el N>0 es para no entrar en el caso [] [] por la linea de arriba y esta.
+distPref([X|XS],[Y|YS],N) :- is_list(YS), entre(0,1,X), entre(0,1,Y), X==Y,  distPref(XS,YS,N).
+distPref([X|XS],[Y|YS],N) :- is_list(YS), entre(0,1,X), entre(0,1,Y), X=\=Y, length(YS,LengthYS), length(XS,LengthXS), binaria(XS,LengthXS), binaria(YS,LengthYS), N is (LengthYS+LengthXS+2).
 
-distPref(X,Y,N)   :- not(ground(Y)), not(ground(N)), desde(0,N), distPref(X,Y,N).
-distPref([],YS,N) :- ground(YS), length(YS,N), binaria(YS,N). %1
-distPref([],YS,N) :- not(ground(YS)), binaria(YS,N). %3 
-distPref(XS, [],N):- length(XS,N), N>0, binaria(XS,N). %4	
-distPref([X|XS],[Y|YS],N) :- entre(0,1,X), entre(0,1,Y), X==Y, distPref(XS,YS,N). %5
-distPref([X|XS],[Y|YS],N) :- entre(0,1,X), entre(0,1,Y), X=\=Y,     ground(YS),  length(XS,LengthXS), binaria(XS,LengthXS), length(YS,LengthYS), binaria(YS, LengthYS),	N is (LengthXS+LengthYS+2). %6
-distPref([X|XS],[Y|YS],N) :- entre(0,1,X), entre(0,1,Y), X=\=Y, not(ground(YS)), length(XS,LengthXS), ACUM is (N-LengthXS-2), distPref([],YS,ACUM).	%8		  
 
 %1 si la lista no instanciada ya tiene una longitud fija, cortamos al superar la cota del resultado.
 %Binaria(?L,+N)
