b519d503737de8e84b460579c593759f65e4acc7
anafelisatti@gmail.com <anafelisatti@gmail.com@e665703e-009a-9b55-7c73-0e67ad5d86b4>
2013-06-06 14:41:16 +0000

    Test de distHam comentados. Agregados nuevos para listas con |.

--- PLP/TP2/distHam.pl
+++ PLP/TP2/distHam.pl
@@ -15,10 +15,28 @@ binaria([Y|YS], N) :- entre(0,1,V), Y is V, ACUM is (N - 1), ACUM >= 0, binaria(
 desde(X,X).
 desde(X,Y) :- Z is X+1, desde(Z,Y).
 
+
+%Tests de instanciación de X
+testDh8A(X) :- distHam([0,1,0],X,2), member(X,[[1,0,0]]),!.
+testDh8B(X) :- distHam([0,1,0],X,2), member(X,[[0,0,1]]),!.
+testDh8C(X) :- distHam([0,1,0],X,2), member(X,[[1,1,1]]),!.
+%Tests de instanciación de X (segunda lista semi instanciada)
+testDh8D(X) :- distHam([0,1,0],[1|X],2), member(X,[[0,0]]),!.
+testDh8E(X) :- distHam([0,1,0],[1|X],2), member(X,[[1,1]]),!.
+testDh8F(X) :- distHam([0,1,0],[0,1,0|X],0), member(X,[[]]),!.
+testDh8G(X) :- not(distHam([0,1,0],[1,0,0,0|X],2)).
+
+%Tests de instanciación de Y y N
 testDh0A(Y,N) :- distHam([0,0],Y,N), member(Y, [[0,0]]), N is 0, !.
 testDh1B(Y,N) :- distHam([0,0],Y,N), member(Y, [[0,1]]), N is 1, !.
 testDh2C(Y,N) :- distHam([0,0],Y,N), member(Y, [[1,0]]), N is 1, !.
 testDh3D(Y,N) :- distHam([0,0],Y,N), member(Y, [[1,1]]), N is 2, !.
+
+%Tests de instanciación de N
+testDh5A(N) :- distHam([0,1],[0,1],N), N is 0,!.
+testDh6A(N) :- distHam([0,1,0],[0,0,1],N), N is 2,!.
+
+%Tests de instanciación de N (entradas invalidas)
 testDh4A(N) :- not(distHam([],[0],N)), !.
 testDh4B(N) :- not(distHam([0],[],N)), !.
 testDh4C :- not(distHam([],[],4)).
@@ -26,17 +44,12 @@ testDh4D(N) :- not(distHam([plp],[plp],N)).
 testDh4E(N) :- not(distHam([1,8],[1,0],N)).
 testDh4F(N) :- not(distHam([1,0],[1,5],N)).
 
-testDh5A(N) :- distHam([0,1],[0,1],N), N is 0,!.
-testDh6A(N) :- distHam([0,1,0],[0,0,1],N), N is 2,!.
-
+%Tests de instanciación de X, Y y N
 testDh7A(X,Y,N) :- distHam([0,1,0],[X,Y,1],N), X is 0, Y is 0, N is 2, !.
 testDh7B(X,Y,N) :- distHam([0,1,0],[X,Y,1],N), X is 0, Y is 1, N is 1, !.
 testDh7C(X,Y,N) :- distHam([0,1,0],[X,Y,1],N), X is 1, Y is 0, N is 3, !.
 testDh7D(X,Y,N) :- distHam([0,1,0],[X,Y,1],N), X is 1, Y is 1, N is 2, !.
 
-testDh8A(X) :- distHam([0,1,0],X,2), member(X,[[1,0,0]]),!.
-testDh8B(X) :- distHam([0,1,0],X,2), member(X,[[0,0,1]]),!.
-testDh8C(X) :- distHam([0,1,0],X,2), member(X,[[1,1,1]]),!.
 
 testAll :- 	call(testDh0A(Y1,N1)),
 			call(testDh1B(Y2,N2)),
@@ -56,4 +69,8 @@ testAll :- 	call(testDh0A(Y1,N1)),
 			call(testDh7D(X12,Y12,N12)),
 			call(testDh8A(N13)),
 			call(testDh8B(N14)),
-			call(testDh8C(N15)).
+			call(testDh8C(N15)),
+			call(testDh8D(N21)),
+			call(testDh8D(N22)),
+			call(testDh8F(N23)),
+			call(testDh8G(N24)).
